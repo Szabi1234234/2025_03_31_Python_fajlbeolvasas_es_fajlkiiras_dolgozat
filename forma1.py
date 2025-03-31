dfbbd28f375f5c991ea391960f28e275c3513c81
Szabi1234234 <toth.szabolcs1@diak.szbi-pg.hu>
2025-03-31 09:07:41 +0200

    solution

--- forma1.py
+++ forma1.py
@@ -14,3 +14,38 @@ print("A beolvasott fájlban összesen ____ versenyző szerepel.")
 print("A legtöbb futamot nyert versenyző: ____")
 print("A legtöbb futamot teljesített versenyző: ____")
 print("Az átlagos futamszám: ____")
+
+versenyzok = []
+with open('beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+        next(forrasfajl)
+        for sor in forrasfajl:
+            adatok = sor.strip().split(';')
+            nev = adatok[0]
+            csapat = adatok[1]
+            gyozelmek_szama = int(adatok[2])
+            teljesitett_futamok_szama =  int(adatok[3])
+
+            versenyzok.append([nev, csapat, gyozelmek_szama, teljesitett_futamok_szama])
+
+print(f'{versenyzok=}')
+#1
+versenyzo_szam = len(versenyzok)
+print(f'A beolvasott fájlban összesen {versenyzo_szam} versenyző szerepel.')
+#2
+zsivany = max(versenyzok, key=lambda x: x[1])
+zsivany = zsivany[1]
+print(f'A legtöbb futamot nyert versenyző: {zsivany}')
+#3
+legtobb_futam = max(versenyzok, key=lambda x: x[3])
+legtobb_futam = legtobb_futam[3]
+print(f'A legtöbb futamot teljesített versenyző: {legtobb_futam}')
+#4
+atlag = sum([x[3] for x in versenyzok]) / len(versenyzok)
+print(f'Az átlagos futamszám: {atlag}')
+
+
+with open('./kiirt_adatok/statisztika.txt', 'w', encoding='utf-8') as celfajl:
+    celfajl.write(f'A beolvasott fájlban összesen {versenyzo_szam} versenyző szerepel.')
+    celfajl.write(f'A legtöbb futamot nyert versenyző: {zsivany}')
+    celfajl.write(f'A legtöbb futamot teljesített versenyző: {legtobb_futam}')
+    celfajl.write(f'Az átlagos futamszám: {atlag}')
